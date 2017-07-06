f085f5bf29b1837644513fa306f3c9e04810e67c
FGDOLIVEIRA <FGDOLIVEIRA@WS-LAB08_132.univel.univel.br>
2017-07-06 20:23:12 -0300

    :)

---
+++ Prova2/src/br/univel/Principal.java
@@ -0,0 +1,20 @@
+package br.univel;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		int ra = 160772;
+		
+		Ex01.execute(ra);
+		Ex02.execute(ra);
+		Ex03.execute(ra);
+		Ex04.execute(ra);
+		Ex05.execute(ra);
+		Ex06.execute(ra);
+		Ex07.execute(ra);
+		Ex08.execute(ra);
+		Ex09.execute(ra);
+		Ex10.execute(ra);
+	}
+}
